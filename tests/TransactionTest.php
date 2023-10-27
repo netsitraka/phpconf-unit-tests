01d102d955cacc1a3555ec2e8b05b1b897021f5e
Sitraka Ratsimba <netsitraka@gmail.com>
2023-10-27 16:43:45 +0300

    04 - refactor tests

--- tests/TransactionTest.php
+++ tests/TransactionTest.php
@@ -7,35 +7,40 @@ namespace Test;
 use PhpConf\Account;
 use PhpConf\Notifier;
 use PhpConf\Transaction;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class TransactionTest extends TestCase
 {
-    public function testTransferShouldMoveMoneyFromAnAccountToAnother(): void
+    private Account $from;
+    private Account $to;
+    private Notifier|MockObject $notifier;
+    private Transaction $transaction;
+
+    protected function setUp(): void
     {
         // Arrange
-        $from = new Account('account1@example.com', 500);
-        $to = new Account('account2@example.com', 400);
-        $notifier = $this->createMock(Notifier::class);
-        $transaction = new Transaction($notifier);
+        $this->from = new Account('account1@example.com', 500);
+        $this->to = new Account('account2@example.com', 400);
+        $this->notifier = $this->createMock(Notifier::class);
 
+        $this->transaction = new Transaction($this->notifier);
+    }
+
+    public function testTransferShouldMoveMoneyFromAnAccountToAnother(): void
+    {
         // Act
-        $transaction->transfer(100, $from, $to);
+        $this->transaction->transfer(100, $this->from, $this->to);
 
         // Assert
-        $this->assertEquals(400, $from->getBalance());
-        $this->assertEquals(500, $to->getBalance());
+        $this->assertEquals(400, $this->from->getBalance());
+        $this->assertEquals(500, $this->to->getBalance());
     }
 
     public function testTransferShouldNotifyDebtorAndCreditorAboutTheTransaction(): void
     {
-        $from = new Account('account1@example.com', 500);
-        $to = new Account('account2@example.com', 400);
+        $this->notifier->expects($this->exactly(2))->method('notify');
 
-        $notifier = $this->createMock(Notifier::class);
-        $notifier->expects($this->exactly(2))->method('notify');
-
-        $transaction = new Transaction($notifier);
-        $transaction->transfer(100, $from, $to);
+        $this->transaction->transfer(100, $this->from, $this->to);
     }
 }
